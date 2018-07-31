b4b5cea4d7de3970dd8f99b5e9b066a41c347942
Darkchinobi <stalindesarrollador@gmail.com>
2018-07-31 06:43:43 -0500

    Se montan los archivos al Git
    Se monta el proyecto a Git

---
+++ js/efectos.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  
+  $('.salir, .logoMenu').click(function(){
+    $('.mascara').fadeOut('slow');
+  });
+
+  $('.quienSomos').click(function(){
+    $('#formPqr').fadeOut('slow');
+    $('#somos').toggle('bounce');
+  });
+
+  $('.menuIngles').click(function(){
+    $('#formPqr').fadeOut('slow');
+    $('#ingles').toggle('clip');
+  });
+  $('.menuFran').click(function(){
+    $('#formPqr').fadeOut('slow');
+    $('#frances').toggle('fold');
+  });
+  $('.menuPqr').click(function(){
+    $('#formPqr').toggle('clip');
+  });
+
+  $('#btnGrupal').click(function(){
+    $('#grupal').toggle('highlight');
+  });
+
+  $('#btnPer').click(function(){
+    $('#personal').toggle('puff');
+  });
+
+  $('#btnInt').click(function(){
+    $('#internacional').toggle('pulsate');
+  });
+
+  $('#btnSk').click(function(){
+    $('#skype').toggle('scale');
+  });
+
+  $('#btnApoyo').click(function(){
+    $('#apoyo').toggle('shake');
+  });
+
+  $('#btnIc').click(function(){
+    $('#icfes').toggle('size');
+  });
+
+  $('#btnVaca').click(function(){
+    $('#vacacional').toggle('slide');
+  });
+
+  $('#btnPis').click(function(){
+    $('#pis').toggle('bounce');
+  });
+
+  $('#btnCon').click(function(){
+    $('#conversacion').toggle('scale');
+  });	
+});
